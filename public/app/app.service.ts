a4b308be679d91f38743fbc7856bad757690ae22
venkaiahvempalla <vempallavenkaiah@gmail.com>
2017-12-23 23:13:17 +0530

    angular2 project

---
+++ public/app/app.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http , Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+//import { IProduct } from './product';
+
+@Injectable()
+export class ProductService {
+   private _producturl='/get/data';
+   constructor(private _http: Http){}
+   
+   getproducts(): Observable<IProduct[]> {
+      return this._http.get(this._producturl)
+      .map((response: Response) => <name[]> response.json())
+      .do(data => console.log(JSON.stringify(data)));
+   }
+}
+
+
